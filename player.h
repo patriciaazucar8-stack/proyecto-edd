84181321e32e3b1fb37ad32e6a7b92f35b8f92f5
Paula H. Lifschitz <168595683+paularchve@users.noreply.github.com>
2025-11-16 20:59:06 -0300

    Refactor player.h to remove duplicates
    Removed redundant player struct definition and includes.

--- player.h
+++ player.h
@@ -13,19 +13,3 @@ typedef struct {
 void inicializarJugador(Jugador *j, const char *nombre, const Pokemon *p, int esCPU);
 
 #endif
-#ifndef PLAYER_H
-#define PLAYER_H
-
-#include "pokemon.h"
-
-// Representa a un jugador (nombre y Pokémon seleccionado)
-typedef struct {
-    char nombre[64];
-    Pokemon pokemon;
-    int esCPU; // 0 = humano, 1 = CPU
-} Jugador;
-
-// Crea/Inicializa un jugador a partir de nombre y prototipo de Pokémon
-void inicializarJugador(Jugador *j, const char *nombre, const Pokemon *proto, int esCPU);
-
-#endif
